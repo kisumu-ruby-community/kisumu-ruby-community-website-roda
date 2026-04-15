a038593344a7b982ad3d0fd50d8a7e35724ba3b2
abrakingoo <maikoabraham68@gmail.com>
2026-04-16 00:20:07 +0300

    Fix events migration to use event_type column name instead of reserved word type

--- db/002_create_events.rb
+++ db/002_create_events.rb
@@ -2,16 +2,19 @@ Sequel.migration do
   up do
     create_table?(:events) do
       column :id, :uuid, primary_key: true, default: Sequel.function(:gen_random_uuid)
-      String  :title,       null: false
-      String  :description, text: true
-      String  :type
+      String :title, null: false
+      Text :description
+      String :event_type
       DateTime :date
-      String  :location
-      String  :cover_image
-      String  :status,      default: "draft"
-      column  :created_by, :uuid
+      String :location
+      String :cover_image
+      String :status, default: "upcoming"
+      column :created_by, :uuid
       DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
     end
   end
-  down { drop_table(:events) }
+
+  down do
+    drop_table(:events)
+  end
 end
