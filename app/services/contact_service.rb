43a522c292807b55e616f3a136f8510387b54c6e
abrakingoo <maikoabraham68@gmail.com>
2026-04-15 13:42:37 +0300

    Add ContactService to supply data for the contact page

---
+++ app/services/contact_service.rb
@@ -0,0 +1,8 @@
+class ContactService
+    def self.call(r)
+        {
+            title: "Contacts Page",
+            description: "This Page will list all of our contacts and channels of communication"
+        }
+    end
+end
