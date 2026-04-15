ca92e0b53a8dc917866741d3e0e3ff31013be084
abrakingoo <maikoabraham68@gmail.com>
2026-04-15 13:43:07 +0300

    Add main Roda application class with render, public, json, and all_verbs plugins and route definitions for root, about, and contact

---
+++ app.rb
@@ -0,0 +1,24 @@
+require "roda"
+require_relative "app/routes/home"
+require_relative "app/routes/about"
+require_relative "app/routes/contact"
+
+class App < Roda
+    plugin :render, views: "app/views"
+    plugin :public
+    plugin :json
+    plugin :all_verbs
+
+    route do |r|
+        r.public
+        r.root do
+            view("pages/index", locals: Routes::HomeRoute.call(r))
+        end
+        r.on "about" do
+            view("pages/about", locals: Routes::AboutRoute.call(r))
+        end
+        r.on "contact" do
+            view("pages/contact", locals: Routes::ContactRoute.call(r))
+        end
+    end
+end
