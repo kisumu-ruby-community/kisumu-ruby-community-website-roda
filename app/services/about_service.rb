750243bd3bb67a9b9516e2cb805a1c37508bd7a8
abrakingoo <maikoabraham68@gmail.com>
2026-04-15 13:42:31 +0300

    Add AboutService to supply data for the about page

---
+++ app/services/about_service.rb
@@ -0,0 +1,8 @@
+class AboutService
+    def self.call(r)
+    {
+        title: "About Kisumu Ruby Community",
+        description: "We are a community of ruby developers in Kisumu, Kenya. We meet every month to share knowledge and network."
+    }
+    end
+end
