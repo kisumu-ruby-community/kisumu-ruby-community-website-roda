6bf374642ba174fc74fdd227108d557b39ada282
John <johneliud4@gmail.com>
2026-04-15 16:16:18 +0300

    feat: add Formspree integration to contact service for handling form submissions and redirects.

--- app/services/contact_service.rb
+++ app/services/contact_service.rb
@@ -1,8 +1,23 @@
+require "net/http"
+require "json"
+require "uri"
+
 class ContactService
-    def self.call(r)
-        {
-            title: "Contacts Page",
-            description: "This Page will list all of our contacts and channels of communication"
-        }
-    end
+  CONTACT_URL  = ENV.fetch("FORMSPREE_CONTACT_URL")
+  PROPOSAL_URL = ENV.fetch("FORMSPREE_PROPOSAL_URL")
+
+  def self.call(r)
+    form = r.params
+    url  = form["_form"] == "proposal" ? PROPOSAL_URL : CONTACT_URL
+    payload = form.reject { |k, _| k.start_with?("_") }
+
+    uri  = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json", "Accept" => "application/json")
+    req.body = payload.to_json
+    http.request(req)
+
+    r.redirect "/contact?sent=#{form["_form"] == "proposal" ? "proposal" : "contact"}"
+  end
 end
