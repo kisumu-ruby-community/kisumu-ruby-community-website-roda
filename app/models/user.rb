f904060b3122084d09f2decf8390a0e12a46afe6
John <johneliud4@gmail.com>
2026-04-15 21:51:03 +0300

    refactor: update User model with GitHub OAuth authentication, role-based methods, and visitor role assignment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
-require "bcrypt"
-
 class User < Sequel::Model
-  def password=(new_password)
-    self.password_digest = BCrypt::Password.create(new_password)
-  end
+  def admin?  = role == "admin"
+  def member? = role == "member" || admin?
 
-  def authenticate(password)
-    BCrypt::Password.new(password_digest) == password
+  def self.from_github(auth)
+    user = first(github_id: auth["uid"].to_s) ||
+           first(github_username: auth.dig("info", "nickname"))
+
+    attrs = {
+      github_id:       auth["uid"].to_s,
+      github_username: auth.dig("info", "nickname"),
+      name:            auth.dig("info", "name") || auth.dig("info", "nickname"),
+      email:           auth.dig("info", "email") || "",
+      avatar_url:      auth.dig("info", "image"),
+    }
+
+    if user
+      user.update(attrs)
+    else
+      attrs[:role] = "visitor"
+      user = create(attrs)
+    end
+
+    user
   end
 end
