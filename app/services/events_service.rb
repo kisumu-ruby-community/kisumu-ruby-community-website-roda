1160fc02584d9a3dba000cd8bc4d1c3513421e46
John <johneliud4@gmail.com>
2026-04-16 07:58:49 +0300

    feat(events): add formatted RSVP count display using DisplayCount utility for event detail page.

--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -1,3 +1,5 @@
+require_relative "../../lib/display_count"
+
 class EventsService
   def self.index
     all     = Event.order(:date).all
@@ -11,7 +13,7 @@ class EventsService
     return { title: "Event not found", event: nil, speakers: [], rsvp_count: 0, user_rsvpd: false } unless event
 
     speakers   = EventSpeaker.where(event_id: event.id).all
-    rsvp_count = DB[:rsvps].where(event_id: event.id).count
+    rsvp_count = DisplayCount.call(DB[:rsvps].where(event_id: event.id).count)
     user_rsvpd = current_user && DB[:rsvps].first(event_id: event.id, user_id: current_user.id)
 
     { title: event.title, event: event, speakers: speakers,
