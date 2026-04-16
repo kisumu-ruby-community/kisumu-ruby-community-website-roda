378fa00638921d87b1ca9b777660b6f13dc84cc5
abrakingoo <maikoabraham68@gmail.com>
2026-04-16 13:29:53 +0300

    Rewrite rack_attack.rb cleanly with a proper in-process memory store.

--- config/rack_attack.rb
+++ config/rack_attack.rb
@@ -1,5 +1,40 @@
 require "rack/attack"
 
+Rack::Attack.cache.store = Class.new do
+  def initialize
+    @data  = {}
+    @mutex = Mutex.new
+  end
+
+  def increment(key, amount, options = {})
+    @mutex.synchronize do
+      expires_in = options[:expires_in] || 60
+      entry = @data[key]
+      if entry.nil? || entry[:expires_at] < Time.now
+        @data[key] = { value: amount, expires_at: Time.now + expires_in }
+      else
+        @data[key][:value] += amount
+      end
+      @data[key][:value]
+    end
+  end
+
+  def read(key)
+    @mutex.synchronize do
+      entry = @data[key]
+      return nil if entry.nil? || entry[:expires_at] < Time.now
+      entry[:value]
+    end
+  end
+
+  def write(key, value, options = {})
+    @mutex.synchronize do
+      expires_in = options[:expires_in] || 60
+      @data[key] = { value: value, expires_at: Time.now + expires_in }
+    end
+  end
+end.new
+
 # 5 auth attempts per IP per minute
 Rack::Attack.throttle("auth/ip", limit: 5, period: 60) do |req|
   req.ip if req.path.start_with?("/auth/")
